a7c557e27fe7b45b634d7a8607a9f3873eee953b
adarshnambiar <adarshnambiar@gmail.com>
2015-02-24 14:18:14 -0500

    Scripts
    Edited

--- Blender/export-model.py
+++ Blender/export-model.py
@@ -4,6 +4,6 @@ for item in bpy.data.objects:
               for face in item.data.polygons: 
                   verts_in_face = face.vertices[:]
                   print("face index ", face.index)
+                  print("Vertices in face:",len(verts_in_face))
                   for vert in face.vertices:
-                      print("Vertices in face:",len(verts_in_face))
                       print("Vertex(x,y,z) ", item.data.vertices[vert].co)
